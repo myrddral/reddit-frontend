05e951af6c2fe931c5a334d31276e5e5fcb1c146
Attila Beli <thirdeye0@gmail.com>
2021-03-15 12:08:47 +0100

    page gets redirected to homepage after submitting a post | loading animation added

--- src/Posts.js
+++ src/Posts.js
@@ -4,59 +4,77 @@ import db from "./db.js";
 
 const Posts = () => {
   const [posts, setPosts] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isPending, setIsPending] = useState(true);
 
   const timePassed = (postTimestamp) => {
-    const actualTimestamp = new Date().getTime()/1000;
-    return Math.round((actualTimestamp - postTimestamp)/3600);
-  }
-  
+    const actualTimestamp = new Date().getTime() / 1000;
+    return Math.round((actualTimestamp - postTimestamp) / 3600);
+  };
+
   useEffect(() => {
-    db.collection('posts')
-    .get()
-    .then((data) => {
-      const result = data.docs.map((doc) => ({id: doc.id, ...doc.data()}))
-      setPosts(result);
-      setIsLoading(false);
-      console.log(result);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    db.collection("posts")
+      .get()
+      .then((data) => {
+        const result = data.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        setPosts(result);
+        setIsPending(false);
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
-  
+
   return (
     <>
-      {isLoading && <center><div className="lds-ring"><div></div><div></div><div></div><div></div></div></center>}
-      {posts && posts.map((post) => (
-        <div key={post.id} className="post-container">
-          <div className="vote-container">
-            <Votes votecount={post.score} owner={post.owner} vote={post.vote}/>
+      {isPending && (
+        <center>
+          <div className="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
-          <div className="post-text-container">
-          <div className="post-text">
-            <small className="posted-by">Posted by {post.owner} {timePassed(post.timestamp)} hours ago</small>
-            <h3>
-              {post.title}
-            </h3>
-          </div>
-            <div className="post-actions">
-              <small>123 Comments</small>
-              <small>Award</small>
-              <small>Share</small>
-              <small>Save</small>
+        </center>
+      )}
+      {posts &&
+        posts.map((post) => (
+          <div key={post.id} className="post-container">
+            <div className="vote-container">
+              <Votes
+                votecount={post.score}
+                owner={post.owner}
+                vote={post.vote}
+              />
+            </div>
+            <div className="post-text-container">
+              <div className="post-text">
+                <small className="posted-by">
+                  Posted by {post.owner}{" "}
+                  <span className="timestamp">
+                    {timePassed(post.timestamp)} hours ago
+                    <span className="tooltiptext">{new Date(post.timestamp).toLocaleString()}</span>
+                    </span>
+                </small>
+                <h4>{post.title}</h4>
+              </div>
+              <div className="post-actions">
+                <small>123 Comments</small>
+                <small>Award</small>
+                <small>Share</small>
+                <small>Save</small>
+              </div>
+            </div>
+            <div className="post-image">
+              <img
+                className="post-image"
+                alt="post-content"
+                src={post.url}
+                style={{ height: 110, width: 140 }}
+              />
             </div>
           </div>
-          <div className="post-image">
-            <img
-              className="post-image"
-              alt="post-content"
-              src={post.url}
-              style={{height: 110, width:140}}
-            />
-          </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 };
--- src/SubmitPost.js
+++ src/SubmitPost.js
@@ -1,29 +1,37 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 import "./SubmitPost.css";
-import db from './db';
+import db from "./db";
 
 const SubmitPost = () => {
-  const [title, setTitle] = useState('');
-  const [url, setUrl] = useState('');
-  const [owner, setOwner] = useState('anonymous');
+  const [isPending, setIsPending] = useState(false);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [owner, setOwner] = useState("anonymous");
   const [score, setScore] = useState(0);
   const [timestamp, setTimestamp] = useState(Math.floor(Date.now() / 1000));
   const [vote, SetVote] = useState(0);
+  const [comments, setComments] = useState([]);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = { title, url, owner, score, timestamp, vote }
-    console.log(formData);
-
-    db.collection('posts').doc().set(formData)
-    .then(() => {
-        console.log("Document successfully written!");
-    })
-    .catch((error) => {
-        console.error("Error writing document: ", error);
-    });
-    
+    const formData = { title, url, owner, score, timestamp, vote, comments };
+    setIsPending(true);
+    setTimeout(() => {     
+        db.collection("posts")
+          .doc()
+          .set(formData)
+          .then(() => {
+            console.log("Document successfully written!");
+          })
+          .catch((error) => {
+            console.error("Error writing document: ", error);
+          });
+        setIsPending(false);
+        history.push('/');
+    }, 1000);
   };
 
   return (
@@ -50,12 +58,26 @@ const SubmitPost = () => {
           value={url}
           onChange={(e) => setUrl(e.target.value)}
         ></input>
+        <div className="button-container">
         <Link to="/">
           <button id="cancel-button">CANCEL</button>
         </Link>
-        <button type="submit" id="submit-button">
-          SUBMIT
-        </button>
+        {!isPending && (
+          <button type="submit" id="submit-button">
+            SUBMIT
+          </button>
+        )}
+        {isPending && (
+          <button type="submit" id="submit-button-pending" disabled>
+            <div className="lds-ellipsis">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </button>
+        )}
+        </div>
       </form>
     </div>
   );
