8b4115d9612754a18f62cb1362f298f5db97748e
Attila Beli <thirdeye0@gmail.com>
2021-03-18 23:22:40 +0100

    cleaned up comment section

--- src/CommentSection.js
+++ src/CommentSection.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouteMatch } from "react-router";
 import { useAuth } from "./auth.js";
 import { v4 as uuidv4 } from "uuid";
@@ -7,20 +7,29 @@ import db from "./db";
 
 const CommentSection = ({ comments }) => {
   const { currentUser } = useAuth();
+  const [commentList, setCommentList] = useState(comments)
   const [isPending, setIsPending] = useState(false);
-  const [comment, setComment] = useState('');
+  const [comment, setComment] = useState("");
   const timestamp = Math.floor(Date.now() / 1000);
-  const commenter = currentUser.displayName;
+  let commenter = "";
   let match = useRouteMatch();
+
+  if (currentUser) {
+    commenter = currentUser.displayName;
+  }
 
   const timePassed = (postTimestamp) => {
     const actualTimestamp = new Date().getTime() / 1000;
     return Math.round((actualTimestamp - postTimestamp) / 3600);
   };
 
+  useEffect(() => {
+    setCommentList(comments)
+  }, [comments]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    
+
     const uuid = uuidv4().replace(/-/g, "");
 
     const commentKey = `comments.${uuid}.comment`;
@@ -32,8 +41,8 @@ const CommentSection = ({ comments }) => {
       [commentKey]: comment,
       [commenterKey]: commenter,
       [timestampKey]: timestamp,
-      [likedbyKey]: []
-    }
+      [likedbyKey]: [],
+    };
 
     setIsPending(true);
     db.collection("posts")
@@ -47,36 +56,37 @@ const CommentSection = ({ comments }) => {
       });
     setIsPending(false);
   };
-  
-  console.log(this);
+
   return (
     <>
       <div className="comment-section">
-        <form onSubmit={handleSubmit} className="submit-comment">
-          <div className="userinfo">
-            <label htmlFor="new-comment">
-              Comment as{" "}
-              <small>
-                <strong>{currentUser.displayName}</strong>
-              </small>
-            </label>
-          </div>
-          <textarea
-            name="new-comment"
-            className="new-comment-textarea"
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-          ></textarea>
-          {!isPending && (
-            <button type="submit" id="submit-comment-button">
-              Comment
-            </button>
-          )}
-        </form>
+        {currentUser !== null && (
+          <form onSubmit={handleSubmit} className="submit-comment">
+            <div className="userinfo">
+              <label htmlFor="new-comment">
+                Comment as{" "}
+                <small>
+                  <strong>{currentUser.displayName}</strong>
+                </small>
+              </label>
+            </div>
+            <textarea
+              name="new-comment"
+              className="new-comment-textarea"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            ></textarea>
+            {!isPending && (
+              <button type="submit" id="submit-comment-button">
+                Comment
+              </button>
+            )}
+          </form>
+        )}
         <div className="comments-list">
-          {comments &&
+          {commentList &&
             // entry[0] === comment key | entry[1] === comment data
-            Object.entries(comments).map((entry) => (
+            Object.entries(commentList).map((entry) => (
               <div key={entry[0]} className="comment-container">
                 <small className="commenter">
                   <strong>{entry[1].commenter} </strong>
